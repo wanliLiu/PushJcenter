8e5c7a5535da6400d73d1348804b729f695cce14
Sofia4041 <353124127@qq.com>
2018-01-05 00:10:35 +0800

    bintrayUpload

---
+++ libone/src/main/java/com/soli/libone/test/TestTwo.java
@@ -0,0 +1,18 @@
+package com.soli.libone.test;
+
+/**
+ * Created by soli on 04/01/2018.
+ */
+
+public class TestTwo {
+
+    /**
+     * http://kvh.io/cn/tags/EmbraceAndroidStudio/
+     * 拥抱 Android Studio 之四：Maven 仓库使用与私有仓库搭建
+     * http://blog.bugtags.com/2016/01/27/embrace-android-studio-maven-deploy/
+     */
+
+    public void test() {
+
+    }
+}
