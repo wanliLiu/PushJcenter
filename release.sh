60f6b8c3588764abbd2791415d761a987917a04d
Sofia4041 <353124127@qq.com>
2018-05-26 12:21:50 +0800

    jcenter bintrayUpload

--- release.sh
+++ release.sh
@@ -3,10 +3,11 @@
 #rm -rf $HOME/.gradle/caches
 #rm -rf .gradle
 
+echo "BINTRAY_USERNAME：$BINTRAY_USERNAME"
+echo "BINTRAY_API_KEY：$BINTRAY_API_KEY"
+
 echo "开始编译 上传到jcenter"
 ./gradlew clean build bintrayUpload
-
-#./gradlew clean build -P bintrayUsername=soli -P bintrayApiKey=ebaf0660d0f7ae6f270c9d229510e440d958a26b bintrayUpload
 
 #./gradlew -P repositoryUrl=file://C://Users/Soli/.m2/repository uploadArchives
 
